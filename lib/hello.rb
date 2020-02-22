b0d0aed8958935367a787e0008b22eecb3533d53
ndudo@worcester.edu <ndudo@worcester.edu>
2020-02-22 15:39:45 -0500

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,3 +2,10 @@ Hello,World
 Hello, #{ARGV.first}!
 name = ARGV.first || world
 Hello, #{name}!
+require 'greeter'
+
+# Default is World
+name = ARGV.first || "World"
+
+greeter = Greeter.new(name)
+puts greeter.greet
