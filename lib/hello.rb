2abf0405b2842c7792c684d6d4d997fd4a659aeb
ndudo@worcester.edu <ndudo@worcester.edu>
2020-02-23 17:11:03 -0500

    final version

--- lib/hello.rb
+++ lib/hello.rb
@@ -9,3 +9,26 @@ name = ARGV.first || "World"
 
 greeter = Greeter.new(name)
 puts greeter.greet
+<<<<<<< HEAD
+require 'greeter'
+
+# Default is World
+name = ARGV.first || "World"
+
+greeter = Greeter.new(name)
+puts greeter.greet
+=======
+# Default is World
+
+puts "What's your name"
+my_name = gets.strip
+
+puts "Hello, #{my_name}!"
+>>>>>>> master
+require 'greeter'
+
+puts "What's your name"
+my_name = gets.strip
+
+greeter = Greeter.new(my_name)
+puts greeter.greet
